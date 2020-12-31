326d8cfce5d7ba8529246cbf63f9842b3d064255
Lorenzo Sabbatini <lorenzo.sabbatini@exprivia.com>
2020-12-31 11:51:31 +0100

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello, it's the last day of the Year
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
